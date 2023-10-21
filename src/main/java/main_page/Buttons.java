c1aaa5703633fe7f257b78087020e330064f6df9
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2023-10-21 17:36:10 +0530

    commit

--- src/main/java/main_page/Buttons.java
+++ src/main/java/main_page/Buttons.java
@@ -73,7 +73,7 @@ public static void main(String[] args){
         //Click Image Button and Click on any hidden button
         ButtonPom.Hidden.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        ButtonPom.Hidden.click();
+        ButtonPom.Hidden.click();f
 
 
     //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
