c84db5e1e6f5f1e99afd869dd04dcf57f8791b63
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2023-10-26 10:49:18 +0100

    Update and rename CheckBox to CheckBox.java

---
+++ src/main/java/main_page/CheckBox.java
@@ -0,0 +1,25 @@
+
+ package main_page;
+  
+  import org.openqa.selenium.By;
+  import org.openqa.selenium.Dimension;
+  import org.openqa.selenium.WebElement;
+  import org.openqa.selenium.interactions.Actions;
+  import org.openqa.selenium.support.Color;
+  import org.openqa.selenium.support.PageFactory;
+  import test_pom.ButtonPom;
+  import utils.DriverSetup;
+  import utils.ReadProperties;
+  
+  import java.time.Duration;
+  import java.util.List;
+  import java.util.ListIterator;
+  
+  import static org.openqa.selenium.support.Color.fromString;
+  
+  
+  public class Buttons extends DriverSetup {
+  
+  
+  }
+}
