d482c2d45de624f3922e6ad0ecbc7909f9bfad3c
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2023-11-21 18:52:42 +0530

    new

--- src/main/java/main_page/CheckBox.java
+++ src/main/java/main_page/CheckBox.java
@@ -4,6 +4,7 @@ package main_page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.browser.model.WindowID;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.PageFactory;
@@ -12,6 +13,7 @@ import utils.DriverSetup;
 import utils.ReadProperties;
 
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -28,29 +30,52 @@ WebElement basic = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt89']//foll
     basic.click();
 
 //Notification
-WebElement notify = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt91_input']"));
+WebElement notify = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt91']//following-sibling::div"));
 notify.click();
 
 //Choose your favorite language(s)
-WebElement lang = driver.findElement(By.xpath("//*[@id='j_idt87:basic']//tr//input"));
-lang.click();
+List<WebElement> lang = driver.findElements(By.xpath("//*[@id='j_idt87:basic']//following-sibling::div"));
+    for (WebElement langs : lang){
+        langs.click();
+        break;
+    }
 
-////Tri State Checkbox
-//WebElement tri = driver.findElement(By.xpath("//*[@id='j_idt87:ajaxTriState']//input"));
-//tri.click();
-//
-////Toggle Switch
-//WebElement toggle = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt100_input']"));
-//toggle.click();
-//
-////Verify if check box is disabled
-//WebElement verify = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt102_input']"));
-//verify.isEnabled();
-//
-////Select Multiple
-//WebElement multi = driver.findElement(By.xpath("//*[@class='ui-selectcheckboxmenu-items-wrapper']//li"));
-//multi.click();
 
- 
+//Tri State Checkbox
+WebElement tri = driver.findElement(By.xpath("//*[@id='j_idt87:ajaxTriState']//following-sibling::div"));
+tri.click();
+tri.click();
+
+
+//Toggle Switch
+WebElement toggle = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt100']//following-sibling::div"));
+toggle.click();
+
+//Verify if check box is disabled
+WebElement verify = driver.findElement(By.xpath("//*[@id='j_idt87:j_idt102_input']"));
+Boolean ver = verify.isEnabled();
+System.out.println(ver);
+
+//
+//Select Multiple
+WebElement multi = driver.findElement(By.xpath("//*[@id='j_idt87:multiple']/div[3]/span"));
+multi.click();
+
+Thread.sleep(500);
+List<WebElement> ls_coun = driver.findElements(By.xpath("//*[@id='j_idt87:multiple_panel']/div/ul/li/div/div[2]"));
+
+int count = ls_coun.size();
+System.out.println(count);
+
+for (int i=0 ; i<ls_coun.size();i++){
+    if (i<2){
+        WebElement ls = ls_coun.get(i);
+        ls.click();
+
+    }
+}
+
+
 }
 }
+
